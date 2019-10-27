d8f7930f4fec313db3d2fd152ee3b007cbcd62f9
Giorgi Lekveishvili <lekva@Giorgis-MBP-3.Dlink>
2019-10-27 22:41:34 +0400

    Unit tests: container/list

--- containers/list.h
+++ containers/list.h
@@ -3,19 +3,31 @@
 
 #include <stddef.h>
 
+// Element destructor function.
 typedef void(*FreeFn)(void*);
 
 typedef struct {
+  // Pointer the the continuous memory used to store list items. 
   void* elems;
+  // Size of each element in bytes.
   size_t elem_size;
+  // Element destructor.
   FreeFn free_fn;
+  // Number of elements in the list.
   int size;
+  // Represents how many elements can list hold at the moment.
   int alloc_len;
 } List;
 
+// Initializes new list where each element takes |elem_size| number of bytes.
+// |free_fn| function will be invoked with points to each element during list
+// destruction.
 void ListInit(List* list, size_t elem_size, FreeFn free_fn);
+// Cleans up memory used be the |list|.
 void ListDispose(List* list);
+// Adds new element to the |list| and copies contents of |elem| into it.
 void ListAdd(List* list, const void* elem);
+// Returns pointer to |index|-th element from the list.
 void* ListGet(List* list, int index);
 
 #endif // GL_CONTAINERS_LIST_H_
---
+++ containers/list_test.c
@@ -0,0 +1,70 @@
+#include "gl/containers/list.h"
+
+#include <stdlib.h>
+
+#include "gl/lunit/lunit.h"
+
+TEST(Integers) {
+  List list;
+  ListInit(&list, sizeof(int), /*free_fn=*/NULL);
+  int num_elems = 10;
+  for (int i = 0; i < num_elems; ++i) {
+    ListAdd(&list, &i);
+  }
+  ASSERT_INT_EQ(list.size, num_elems);
+  for (int i = 0; i < num_elems; ++i) {
+    ASSERT_INT_EQ(*(int*)ListGet(&list, i), i);
+  }
+  ListDispose(&list);
+}
+
+void StrFree(void* elem) {
+  free(*(char**)elem);
+}
+
+TEST(Strings) {
+  List list;
+  ListInit(&list, sizeof(char*), StrFree);
+  int num_elems = 2;
+  char* elems[] = {"foo", "bar"};
+  for (int i = 0; i < num_elems; ++i) {
+    char* elem = strdup(elems[i]);
+    ListAdd(&list, &elem);
+  }
+  ASSERT_INT_EQ(list.size, num_elems);
+  for (int i = 0; i < num_elems; ++i) {
+    ASSERT_STR_EQ(*(char**)ListGet(&list, i), elems[i]);
+  }
+  ListDispose(&list);
+}
+
+int* elems_freed = NULL;
+
+void ElemFree(void* elem) {
+  ++elems_freed[*(int*)elem];
+}
+
+TEST(Free) {
+  List list;
+  ListInit(&list, sizeof(int), ElemFree);
+  int num_elems = 10;
+  for (int i = 0; i < num_elems; ++i) {
+    ListAdd(&list, &i);
+  }
+  ASSERT_INT_EQ(list.size, num_elems);
+  elems_freed = malloc(num_elems * sizeof(int));
+  memset(elems_freed, 0, num_elems * sizeof(int));
+  ListDispose(&list);
+  for (int i = 0; i < num_elems; ++i) {
+    ASSERT_INT_EQ(elems_freed[i], 1);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  LUnitOpts opts;
+  LUnitOptsInit(&opts, argc, argv);
+  RUN_TEST(Integers, &opts);
+  RUN_TEST(Strings, &opts);
+  RUN_TEST(Free, &opts);
+  return 0;
+}
